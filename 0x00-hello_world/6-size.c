01d7972b9ae8da1813e364b2aab79b405d19ac4e
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-02 12:54:49 -0700

    Size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
- /**
-  * main - Entry point
-  *
-  * Return: Always 0 (Success)
-  */
+/**
+ * main - Entry point
+ *
+ *Return - Entry point
+ *
+ *Return: Always 0 (Sucess)
+ */
 
 int main(void)
 {
