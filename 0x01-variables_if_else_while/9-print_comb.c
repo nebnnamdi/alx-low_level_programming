e828db1ce1f306b7641e8dd44bc35e6b8dd6feef
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-03 16:22:05 -0700

    Print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Task
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int a = '0';
+
+while (a <= '9')
+{
+putchar(a);
+if (a == '9')
+{
+putchar('\n');
+return (0);
+}
+else
+{
+putchar(',');
+putchar(' ');
+a++;
+}
+}
+return (0);
+}
