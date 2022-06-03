80cdb85d2f80959c0ffd5f2d50dcda98451f3bec
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-03 15:39:07 -0700

    Print numberz without char

---
+++ 0x01-variables_if_else_while/6-print-numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Task
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int x;
+
+for (x = 0; x < 10; x++)
+{
+putchar("%d")
+}
+putchar("\n");
+return (0);
+}
