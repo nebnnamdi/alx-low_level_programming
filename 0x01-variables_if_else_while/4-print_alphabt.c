d7217dda3625c6314df404298bb4602148125bbd
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-03 15:16:44 -0700

    Print incomplete aphlabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints both uppercase and lowercase letters
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+char x = 'a';
+
+for (x = 'a'; x <= 'z'; a++)
+{
+if ((x != 'e') && (x != 'q'))
+}
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
