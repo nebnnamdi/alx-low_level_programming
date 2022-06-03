1eefbf9072725efee439bfb7493388f488f3c36b
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-03 16:00:30 -0700

    Print reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Task
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+char x = 'z';
+
+for (x = 'z'; x > 'a'; x--)
+{
+putchar(x)
+}
+putchar('\n');
+return (0);
+}
