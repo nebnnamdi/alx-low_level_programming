baeb121cb9c68d8c5784990545ad642f30f6adaf
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-07 16:38:42 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+/**
+ * print_last_digit - prints last digin
+ * @x: The input to check
+ * Return: x
+ */
+
+int print_last_digit(int y)
+{
+int x;
+
+x = y % 10;
+
+if (x < 0)
+{
+x *= -1;
+}
+_putchar(x + '0');
+return (x);
+}
