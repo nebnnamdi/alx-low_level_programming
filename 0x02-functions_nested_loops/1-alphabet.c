75a22efefeccc2cdc5c727ce26ac3a118ddad2e1
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-07 09:48:10 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ * Main - Entry
+ *
+ * Print alphabets
+ *
+ */
+
+void print_alphabet(void);
+
+void print_alphabet(void){
+char i = 'a';
+while(i <= 'z'){
+_putchar(i);
+return(0);
+}
+}
