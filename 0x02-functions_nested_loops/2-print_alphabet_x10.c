01469ef98646513e863b4cee99c4428c874adcf0
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-07 14:49:44 -0700

    10 alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,14 @@
 
 void print_alphabet_x10(void)
 {
-char i, l;
+char i, j;
 
-for (l = 0; l <= 9; l++)
+for (j = 0; j <= 9; j++)
 {
 for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
+}
 _putchar('\n');
 }
 }
