8963fb0cda1fdf11ea26ad0c2866823759e137b5
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-07 09:37:48 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,14 @@
  *
  */
 
-int main(void){
-  printf("_putchar");
-  return(0);
+int main(void)
+{
+  char str[] = "_putchar";
+  int ch;
+
+  for (ch = 0; ch < 8; ++ch)
+    _putchar(str[ch]);
+  _putchar('\n');
+
+  return (0);
 }
