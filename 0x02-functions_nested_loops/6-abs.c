26178c152ec611bd990112268e6e8c2ee012ae34
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-07 16:07:56 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_sign - print absolute value of int
+ * @i: The input number to check
+ * Return: int.
+ */
+
+int _abs(int i)
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (-i);
+}
+}
