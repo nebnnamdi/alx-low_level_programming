de5b464b2a3311d9ddd2b1316360c249c0e3caa3
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-08 10:10:01 -0700

    Print n - 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,6 +2,7 @@
 
 /**
  * print_to_98 - prints absolute values of n - 98
+ * @n: inputed number
  */
 
 void print_to_98(int n)
@@ -9,7 +10,7 @@ void print_to_98(int n)
 int x;
 for (x = n; x <= 98; x++)
 {
-_putchar(x);
+_putchar('%d', n);
 _putchar(',');
 _putchar(' ');
 }
