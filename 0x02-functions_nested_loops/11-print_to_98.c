336acafd0600cf882d89d77ac6ba4ad1ac092320
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-08 10:04:31 -0700

    Print n - 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints absolute values of n - 98
+ */
+
+void print_to_98(int n)
+{
+for (x = n; x <= 98; x++)
+{
+  _putchar(x);
+  _putchar(', ');
+}
+return;
+}
