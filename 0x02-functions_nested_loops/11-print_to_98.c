52bca112a98647c9b7847325663b52ded2372296
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-08 14:07:31 -0700

    Print n -98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,18 +1,38 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- * print_to_98 - prints absolute values of n - 98
- * @n: inputed number
+ *  print_to_98 - print numbers from n - 98
+ *  @n: input number
+ *  Return: numbers from n 98
  */
-
 void print_to_98(int n)
 {
-char x;
-for (x = n; x <= 98; x++)
+int i;
+if (n == 98)
 {
-_putchar(x);
-_putchar(',');
-_putchar(' ');
+printf("%d", n);
 }
-return;
+if (n > 98)
+{
+for (i = n; i > 97; i--)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+}
+if (n < 98)
+{
+for (i = n; i < 99; i++)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+}
+printf("%c", '\n');
 }
