c39ef889b369924dac8e18c6a73a180093adb697
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-09 09:26:46 -0700

    Print numbers without 2,4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include 'main.h'
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 without 2,4
+ * Return:
+ */
+
+void print_most_numbers(void)
+{
+int x = 0;
+
+while (x < 10)
+{
+if (x != 2 && x != 4)
+_putchar(x + '0');
+x++;
+}
+_putchar('\n');
+}
