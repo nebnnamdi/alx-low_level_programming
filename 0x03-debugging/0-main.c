3c62b55401856f9d69e1c67e948d881ad29b2d9c
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-09 04:06:10 -0700

    Test function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int x;
+
+x = 0;
+positive_or_negative(x);
+
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This directory contains files on debugging for tasks 0x03-debugging
