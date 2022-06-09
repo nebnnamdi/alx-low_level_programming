a62f7649cb0c78751836290a5efb547d97ac5087
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-09 05:28:42 -0700

    Largest number check

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,22 +12,17 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && b > c)
+if (a >= b && b > c)
 {
 largest = a;
 }
-else if (b > a && a > c)
+else if (b >= a && a > c)
 {
 largest = b;
 }
-else if (c > b)
+else
 {
 largest = c;
 }
-else
-{
-largest = b
-}
-
 return (largest);
 }
