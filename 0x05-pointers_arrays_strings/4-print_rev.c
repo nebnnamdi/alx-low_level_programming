47030b4caa6a68813a3556379eb77c6aac4d7e30
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-13 08:19:13 -0700

    reverse print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,32 +3,25 @@
 /**
  * rev_string - function that reverses a string
  * @s: String to reverse
- * Return:
- */
-
-#include "main.h"
-
-/**
- * rev_string - function that reverses a string
- * @s: String to reverse
+ * Description: reverse printing of a string
  * Return:
  */
 
 void print_rev(char *s)
 {
-  int count = 0;
+int count = 0;
 
-  while (s[count])
-    {
-      count++;
-    }
+while (s[count])
+{
+count++;
+}
 
-  count--;
+count--;
 
-  for (; count >= 0; count--)
-    {
-      _putchar(s[count]);
-    }
+for (; count >= 0; count--)
+{
+_putchar(s[count]);
+}
 
-  _putchar('\n');
+_putchar('\n');
 }
