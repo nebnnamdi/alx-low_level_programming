3f66b9d67b32f3736133f2c1dcd2cb463ea4b2c7
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-20 13:18:24 -0700

    fill mem const with byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_memset - fills memory with a constant byte.
+ *@s: pointer
+ *@b: char
+ *@n: integer
+ *Return: string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int x;
+
+for (x = 0; x < n; x++;)
+{
+char s[n] = b;
+
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Scripts on 0x07. C - Even more pointers, arrays and strings.
