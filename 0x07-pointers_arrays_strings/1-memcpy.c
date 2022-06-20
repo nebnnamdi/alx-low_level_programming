0fdd33a2451edd8672e4577b8d9ab48d8420b954
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-20 13:40:19 -0700

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_memcpy - copies memory
+ *@dest: destination
+ *@src: source
+ *@n: integer
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int x;
+
+for (x = 0; x < n; x++)
+{
+dest[n] = src[n];
+
+return (dest);
+}
