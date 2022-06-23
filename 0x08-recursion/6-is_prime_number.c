91ece28a03051096643da0c755dfa965bc3dd38c
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-23 09:33:45 -0700

    task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,7 +4,7 @@
  * is_prime_number - returns 1 if the input integer is a prime number
  *getNumPrim - function that gets if num is 0 or 1
  *@num: inout number
- *@1: counter variable
+ *@i: counter variable
  * otherwise return 0
  * @n: input number
  * Return: int
@@ -20,13 +20,6 @@ return (0);
 }
 return (getNumPrim(n, 2));
 }
-
-/**
- * getNumPrim - function that gets if num is 0 or 1
- * @num: input number
- * @i: Counter variable
- * Return: int
- */
 
 int getNumPrim(int num, int i)
 {
