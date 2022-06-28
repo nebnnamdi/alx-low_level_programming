039d9f8f02447bacb5777f50c9db3d7fc6c31cbe
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-06-28 04:23:45 -0700

    create an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,12 +9,12 @@
 
 char *create_array(unsigned int size, char c)
 {
-  char *ar;
-  unsigned int i = 0;
+char *ar;
+unsigned int i = 0;
 
 if (size != 0)
 {
-ar = malloc(size *sizeof(char));
+ar = malloc(size * sizeof(char));
 }
 else
 {
