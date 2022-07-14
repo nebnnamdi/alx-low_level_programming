5baa425c5dca238bb6c04a968177c805264f337a
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-07-14 05:34:21 -0700

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stddef.h>
+
 /**
  * free_list - frees a linked list
  * @head: the beginning of the list
@@ -8,12 +9,11 @@
 
 void free_list(list_t *head)
 {
-listint_t *tmp;
-
-while (head != NULL)
+if (head == NULL)
 {
+return;
+}
 free_list(head->next);
 free(head->str);
 free(head);
-}
 }
