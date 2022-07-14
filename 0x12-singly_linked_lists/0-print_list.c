17ead4ec2df5093f5d65403dfc837bf5c232b421
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-07-14 06:14:12 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 #include "lists.h"
 #include <stdlib.h>
+
 /**
- * print_list - prints all elements of a list_t list
- * @h: singly linked list to print
- * Return: number of nodes in the list
+ * print_list - function that prints all the elements of a list_t list
+ * @h: header pointer
+ * Return: number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
-size_t i;
+size_t cont = 0;
 
-for (i = 0; h; i++)
+while (h != NULL)
 {
-printf("[%d] %s\n", h->len, h->str);
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
 h = h->next;
+cont++;
 }
-return (i);
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+cont++;
+}
+}
+return (cont);
 }
