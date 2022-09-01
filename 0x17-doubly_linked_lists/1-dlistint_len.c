6f2fab7bf8c1091918086a899c6836069f2124a5
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-09-01 08:53:00 -0700

    list length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - number of elements on a dlistint_t list
+ * @h: head of the list
+ * Return: elements number
+ **/
+size_t dlistint_len(const dlistint_t *h)
+{
+unsigned int elements_number = 0;
+
+while (h)
+{
+elements_number++;
+h = h->next;
+}
+return (elements_number);
+}
